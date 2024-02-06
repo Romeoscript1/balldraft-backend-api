97131b54ffbd0bc7b48eda1fe5a65296d50e47b5
supersonicwisd1 <supersonicwisdom@gmail.com>
2024-02-06 11:50:18 +0100

    implemented manager control functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class RegisterUserView(GenericAPIView):
+    serializer_class=UserRegisterSerializer
+
+    def post(self, request):
+        user_data=request.data
+        serializer=self.serializer_class(data=user_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user=serializer.data
+            #send email function user['email]
+            return Response({
+                'data':user,
+                'message':f'Welcome {user.first_name}. Thanks for signing up. Check your mail for you passcode.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
