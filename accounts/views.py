2b534bf9aeb547c0e6995bd97b4c2a463b19e662
supersonicwisd1 <supersonicwisdom@gmail.com>
2024-02-06 16:58:14 +0100

    working on verifing email

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from .serializers import UserRegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import send_code_to_user
+from .models import OneTimePassword
 
 
 
@@ -19,8 +20,26 @@ class RegisterUserView(GenericAPIView):
             send_code_to_user(user['email'])
             #send email function user['email]
             return Response({
-                'status': 200,
                 'data':user,
                 'message':f'Welcome {user['first_name']} to Balldraft. Thanks for signing up. Check your mail for you passcode.'
-            }, status=status.HTTP_400_BAD_REQUEST)
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otpcode=request.data.get('otp')
+        try:
+            user_code_obj=OneTimePassword.objects.get(code=otpcode)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified=True
+                user.save()
+                return Response(
+                    {"message":"Account email verified succesfully"},
+                    status=status.HTTP_200_OK
+                )
+            return Response({
+                "message":"otp code is invalid. User already verified"
+            }, status=status.HTTP_204_NO_CONTENT)
+        except OneTimePassword.DoesNotExist:
+            return Response({"message":"passcode not provided"}, status=status.HTTP_404_NOT_FOUND)
