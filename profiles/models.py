dd45639346fd0a3582d8a6ab5627fbe270be0704
Peter Benjamin Ani <benjaminparish6@gmail.com>
2024-06-21 20:05:08 +0100

    created some extra models

--- profiles/models.py
+++ profiles/models.py
@@ -37,3 +37,148 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.username + "'s Profile"
+
+
+
+    
+class Bonus(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    username = models.CharField(null=True,blank=True,max_length=200)
+    ngn_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    reason = models.TextField(blank=True,null=True)
+    seen = models.BooleanField(default=False)
+    user_see = models.BooleanField(default=True)
+    time = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    verified = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'Bonus Sent to {self.username}'
+    
+    def save(self, *args, **kwargs):
+        if self.verified:
+            body = f"""
+            Dear {self.username},
+            {self.reason}
+            Note: You can deposit, withdraw, enter contest, and transfer funds in and out of your balldraft account without any transaction fees.
+            For any issues with our services, please contact balldraft@balldraft.com"""
+            send_email(f"${self.ngn_amount} Free Bonus  | balldraft LTD", body,  self.profile.user.email)
+            
+            ## credit to their account 
+            self.profile.account_balance += self.ngn_amount
+            self.profile.account_balance += self.ngn_amount
+            self.profile.save()
+            
+        super().save(*args, **kwargs)
+    
+class Penalty(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    username = models.CharField(null=True,blank=True,max_length=200)
+    ngn_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    reason = models.TextField(blank=True,null=True)
+    seen = models.BooleanField(default=False)
+    user_see = models.BooleanField(default=True)
+    time = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    verified = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.profile.username} has been givn a penalty of {self.ngn_amount}'
+    
+    def save(self, *args, **kwargs):
+        if self.verified:
+            body = f"""
+            Dear {self.username},
+            {self.reason}
+            
+            . For further complains or enquiry, kindly contact balldraft@balldraft.com"""
+            send_email(f"${self.ngn_amount}  Penalty Withdrawn from your account  | balldraft LTD", body,  self.profile.user.email)
+            
+            ## minus to their account
+            self.profile.available_balance -= self.ngn_amount
+            self.profile.save()
+        
+        super().save(*args, **kwargs)
+  
+  
+class Notification(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    action = models.TextField(blank=True,null=True)
+    time = models.DateTimeField(auto_now_add=True)
+    action_title = models.CharField(max_length=100,blank=True,null=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.profile.username} - {self.action}'
+
+
+
+class Deposit(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=20,decimal_places=5,blank=True,null=True)
+    bank_name  = models.CharField(max_length=50,blank=True,null=True)
+    account_number = models.CharField(max_length=100,blank=True,null=True)
+    ngn_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    verified = models.BooleanField(default=False)
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.profile.user.username} has deposited this {self.amount}-{self.verified}'
+
+    from django.db.models import Sum
+    @classmethod
+    def total_amount(cls):
+        return cls.objects.aggregate(models.Sum('ngn_amount'))['ngn_amount__sum'] or 0
+
+    @classmethod
+    def total_verified_amount(cls):
+        return cls.objects.filter(verified=True).aggregate(Sum('ngn_amount'))['ngn_amount__sum'] or 0
+
+    @classmethod
+    def total_unverified_amount(cls):
+        return cls.objects.filter(verified=False).aggregate(Sum('ngn_amount'))['ngn_amount__sum'] or 0
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
+
+
+
+class Withdraw(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    bank_name  = models.CharField(max_length=50,blank=True,null=True)
+    account_number = models.CharField(max_length=100,blank=True,null=True)
+    ngn_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    verified = models.BooleanField(default=False)
+    comment =  models.CharField(max_length=100,blank=True,null=True)
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.profile.user.username} has withdrawn this {self.amount}-{self.verified}'
+
+    @classmethod
+    def total_amount(cls):
+        return cls.objects.aggregate(models.Sum('ngn_amount'))['ngn_amount__sum']
+    @classmethod
+    def total_verified_amount(cls):
+        return cls.objects.filter(verified=True).aggregate(Sum('amount'))['amount__sum'] or 0
+
+    @classmethod
+    def total_unverified_amount(cls):
+        return cls.objects.filter(verified=False).aggregate(Sum('amount'))['amount__sum'] or 0
+
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
+
+
+
+class TransactionHistory(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    action = models.TextField(blank=True,null=True)
+    time = models.DateTimeField(auto_now_add=True)
+    action_title = models.CharField(max_length=100,blank=True,null=True)
+    category = models.CharField(max_length=50,blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.profile.username} - {self.action}'
