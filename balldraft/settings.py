f77e9d613100bdb6f0a0f53b1481246da9f51786
Peter Benjamin Ani <benjaminparish6@gmail.com>
2024-08-04 12:48:23 +0100

    sailign for meeting

--- balldraft/settings.py
+++ balldraft/settings.py
@@ -12,16 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 environ.Env.read_env(BASE_DIR / '.env')
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['balldraft.com','https://balldraft.com','api.balldraft.com','*']
+ALLOWED_HOSTS = ['https://microservice.balldraft.com','*']
 
 
 # Application definition
@@ -117,6 +115,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://*",
+    "*"
 ]
 
 CORS_ALLOW_METHODS = (
