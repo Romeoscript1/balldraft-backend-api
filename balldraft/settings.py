914e5c917920dc8b2913d20165e226534b2843bd
Ubuntu <ubuntu@ip-172-31-4-202.us-west-1.compute.internal>
2024-10-09 12:18:09 +0000

    wtf

--- balldraft/settings.py
+++ balldraft/settings.py
@@ -179,11 +179,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FROM_EMAIL='benjaminparish6@gmail.com'
 
-
+#balldraft/email_Backend.py
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = os.path.join(BASE_DIR, 'balldraft/email_Backend.py')
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
